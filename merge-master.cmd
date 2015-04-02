bc2fa4348f9a7f9af4ec0714b79a9e0c2a3690f3
ENikS <evgeni@eniks.com>
2015-04-02 00:28:02 -0400

    Updated command

--- merge-master.cmd
+++ merge-master.cmd
@@ -1,4 +1,4 @@
 git subtree push --squash --prefix=openssl-MSBuild origin master
 pause
 
-REM git push heroku 157a66d050d7a6188f243243264c765f18bc85fb956:master --force
+REM git push origin 157a66d050d7a6188f243243264c765f18bc85fb956:master --force
